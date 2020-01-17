3c01c9984fa0be3ed3a854b64f39428f6cf86f0a
MartinZekic <58045471+MartinZekic@users.noreply.github.com>
2020-01-17 21:46:25 +0100

    Add files via upload

---
+++ src/components/komppoziv.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="pozivi">
+</div>
+</template>
+
+<style>
+.pozivi{
+background: rgb(20, 20, 20);
+margin-top: 1%;
+width:2000px;
+height:150px;
+border-bottom:solid 1px white;
+border-top:solid 1px white;
+}
+</style>
---
+++ src/views/Inbox.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<navnside>
+</navnside>
+<div class="appp">
+<a>Inbox</a>
+</div>
+</div>
+</template>
+
+
+
+<script>
+import navnside from '@/components/navnside.vue'
+export default {
+  components: {
+    navnside
+  }
+}
+</script>
